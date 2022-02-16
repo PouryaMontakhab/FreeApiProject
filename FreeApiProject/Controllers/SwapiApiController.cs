dc414d66dd4edbdf8bf8a22f29d4f4bbdad50232
pourya.montakhabii@yahoo.com <name@example.com>
2022-02-16 21:25:02 +0330

    feat: Frist commit

---
+++ FreeApiProject/Controllers/SwapiApiController.cs
@@ -0,0 +1,36 @@
+﻿using FreeApiProject.Models;
+using FreeApiProject.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwapiApiController : ControllerBase
+    {
+        private readonly IHttpClientService _httpClient;
+
+        public SwapiApiController(IHttpClientService httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.AddBaseUrl("https://swapi.dev/api/");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = await _httpClient.GetAsync<Films>($"films");
+            return Ok(query);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var query = await _httpClient.GetAsync<Film>($"films/{id}");
+            return Ok(query);
+        }
+    }
+}
